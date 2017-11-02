6fde236d046d537569f7311dd263970b4b310d64
Jason Yeh <wadejason80@gmail.com>
2017-11-02 16:16:32 -0700

    jason: add save function

--- app/src/main/java/mobile/li/weatherappnew/CityListActivity.java
+++ app/src/main/java/mobile/li/weatherappnew/CityListActivity.java
@@ -11,22 +11,30 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.reflect.TypeToken;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import mobile.li.weatherappnew.model.CityInfo;
+import mobile.li.weatherappnew.util.ModelUtils;
 
 public class CityListActivity extends AppCompatActivity {
 
     private static final int REQ_CODE_CITY_ADDER = 100;
+    private static final String MODEL_CITY_INFO = "city_info";
+
     private List<CityInfo> cities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        loadData();
         setupUI();
     }
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -38,6 +46,7 @@ public class CityListActivity extends AppCompatActivity {
             newCity.code = info.get(1);
             cities.add(newCity);
 
+            ModelUtils.save(this, MODEL_CITY_INFO, cities);
             setupCityUI();
         }
     }
@@ -66,5 +75,12 @@ public class CityListActivity extends AppCompatActivity {
             cityLayout.addView(cityView);
         }
 
+    }
+
+    private void loadData() {
+        List<CityInfo> saveCityInfo = ModelUtils.read(this,
+                MODEL_CITY_INFO,
+                new TypeToken<List<CityInfo>>(){});
+        cities = saveCityInfo == null ? new ArrayList<CityInfo>() : saveCityInfo;
     }
 }
